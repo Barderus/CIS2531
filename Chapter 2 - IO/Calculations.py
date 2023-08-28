94bb3130da5c9b031172e535c0e29346dba212bc
Gabriel dos Reis <dosreisg@dupage.edu>
2023-08-28 11:51:04 -0500

    Chapter 2

---
+++ Chapter 2 - IO/Calculations.py
@@ -0,0 +1,16 @@
+#Calculations
+print(9+2)
+print(9-2)
+print(9*2)
+print(9 / 2)
+print(9 // 2)
+print(9 % 2)
+print(9 ** 2)
+print(10 + 6 / 2)
+print((10 + 6) / 2)
+print(9 - 2.0)
+print(9 - float(2))
+print((10 + 6) \
+    /2)
+greeting = "Hello " + "virtual " + "student."
+print(type(greeting))
---
+++ Chapter 2 - IO/printTest-1.py
@@ -0,0 +1,20 @@
+print("Pythn is fun!")
+print("Python", "is", "fun!")
+# Using ; to print next line without escape character
+print("Printing two in one line"); print("The End!")
+#Escape character
+print("Hello world! \n" + "(Second line:) My name is Gabriel")
+#Variables
+myString = "Hello World"
+myInt = 345
+myFloat = 3.459
+#
+print(f'{myString:>30s}\n{myInt:30d}\n{myFloat:30.3f}')
+#Print the unicode number of "c"
+print(ord('c'))
+#print the character 65 unicode
+print(f'{65:c}')
+#Print percentage
+print(f'{.12:.1%}')
+#Print in different formats
+print(f'{"Decimal number: "}{12}\n{"Binary Number: "}{12:b}\n{"Octogonal number: "}{12:o}\n{"Hexadecimal number: "}{12:x}')
