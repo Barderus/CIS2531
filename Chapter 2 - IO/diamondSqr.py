5bd68f4354e6e7f0490b2b8bd5f0dfc15d77b6e6
Gabriel dos Reis <dosreisg@dupage.edu>
2023-08-28 17:10:57 -0500

    1st commit

---
+++ Chapter 2 - IO/diamondSqr.py
@@ -0,0 +1,17 @@
+'''
+Author: Gabriel dos Reis
+Class: CIS2531 - Intro to Python
+Date: 8/28/2023
+Prog: diamondSqr.py
+Description: Create a drawing of a diamond with the colors blue, black, green, and red.
+Under the design, add the following statement: "A colorful diamond square by Gabriel dos Reis"
+'''
+
+import turtle
+
+NORTH = 90
+SOUTH = 270
+EAST = 0
+WEST = 180
+CENTER_X = 0
+CENTER_Y = 0
--- InputTest.py
+++
@@ -1,7 +0,0 @@
-#Get Input
-
-userName = input("Enter your name: ")
-userAge = int(input("Enter your age: "))
-userMoney = float(input("Enter your wage per hour: "))
-print(userName, userAge, userMoney)
-print(type(userName), type(userAge), type(userMoney))
