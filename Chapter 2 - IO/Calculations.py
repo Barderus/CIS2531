566166741d9ce5722c2e78c7d2d57c6b78dc2bfd
Gabriel dos Reis <dosreisg@dupage.edu>
2023-08-28 11:56:14 -0500

    Update Calculations.py

--- Chapter 2 - IO/Calculations.py
+++ Chapter 2 - IO/Calculations.py
@@ -1,16 +1,27 @@
 #Calculations
+#Sum
 print(9+2)
+#Subtraction
 print(9-2)
+#Multiplication
 print(9*2)
+#Division
 print(9 / 2)
+#Integer division
 print(9 // 2)
+#Remainder
 print(9 % 2)
+#Exponential
 print(9 ** 2)
+#Order of operation
 print(10 + 6 / 2)
 print((10 + 6) / 2)
+#Float numbers
 print(9 - 2.0)
 print(9 - float(2))
+# Multiline
 print((10 + 6) \
     /2)
 greeting = "Hello " + "virtual " + "student."
+#Checking data type
 print(type(greeting))
