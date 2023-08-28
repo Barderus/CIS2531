94d9ca9757da623c3550d3a97512466deba72127
Gabriel dos Reis <90578742+Barderus@users.noreply.github.com>
2023-08-28 12:05:21 -0500

    Add files via upload

---
+++ InputTest.py
@@ -0,0 +1,7 @@
+#Get Input
+
+userName = input("Enter your name: ")
+userAge = int(input("Enter your age: "))
+userMoney = float(input("Enter your wage per hour: "))
+print(userName, userAge, userMoney)
+print(type(userName), type(userAge), type(userMoney))
