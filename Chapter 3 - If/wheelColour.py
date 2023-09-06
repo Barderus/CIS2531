87dc779b79ca79003aae4ba97b8c6faf07d9abc9
Gabriel dos Reis <dosreisg@dupage.edu>
2023-09-06 12:16:37 -0500

    Finished the program and documented as per request

--- Chapter 3 - If/wheelColour.py
+++ Chapter 3 - If/wheelColour.py
@@ -19,3 +19,42 @@ the range of 0 through 36.
 '''
 
 userChoice = int(input("Enter a pocket number: "))
+
+# Identifying the colour of the pocket
+# 0 is always green
+if userChoice == 0:
+    print("Pocket is Green!")   
+elif userChoice >= 1 and userChoice <= 10: # Range 1 through 10 
+    if userChoice % 2 == 1: # Odd numbers = red and even numbers = black
+        print("Pocket is Red!")
+    else:
+        print("Pocket is Black!")
+elif userChoice >= 11 and userChoice <= 18: #Range 11 through 18
+    if userChoice % 2 == 1: #Odd numbers = black and even numbers = red
+        print("Pocket is Black!")
+    else:
+        print("Pocket is Red!")   
+elif userChoice >= 19 and userChoice <= 28: #Range 19 through 28
+    if userChoice % 2 == 1: # Odd numbers = red and even numbers = black
+        print("Pocket is Red!")
+    else:
+        print("Pocket is Black!")
+elif userChoice >= 29 and userChoice <= 36: #Range 29 through 36
+    if userChoice % 2 == 0: #Odd numbers = black and even numbers = red
+        print("Pocket is Red!")
+    else:
+        print("Pocket is Black!")
+else:
+    print("Error! Invalid number! It must be between 0 and 36!")
+
+'''
+*** OUTPUT1: ***
+
+Enter a pocket number: 12
+Pocket is Black!
+
+*** Ouput2: ***
+
+Enter a pocket number: 41
+Error! Invalid number! It must be between 0 and 36!
+'''
