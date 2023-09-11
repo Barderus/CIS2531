40f3676f22af351e7287fc0aebf49e5af661a945
Gabriel dos Reis <dosreisg@dupage.edu>
2023-09-11 11:37:25 -0500

    Ranming file so it is more descriptive.

---
+++ Chapter 3 - Decision/messAround.py
@@ -0,0 +1,12 @@
+userInput = input("Enter 3 characters abbreviation for a winter month: ")
+
+if userInput == "Dec":
+    print("December")
+elif userInput == "Jan":
+    print("January")
+elif userInput == "Feb":
+    print("February")
+else:
+    print("You entered a invalid winter month.")
+
+
