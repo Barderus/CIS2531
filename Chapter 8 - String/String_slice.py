7315d99b25b7823a74ae17c7edfe42da4f8cf4d9
Gabriel dos Reis <dosreisg@dupage.edu>
2024-01-10 09:17:57 -0600

    Fist commit

---
+++ Chapter 8 - String/String_slice.py
@@ -0,0 +1,16 @@
+'''
+    Slicing String
+'''
+
+parrot = "Norwegian Blue"
+
+print(parrot[0:6])
+print(parrot[3:5])
+print(parrot[:9])
+print(parrot[10:])
+
+print(parrot[:6] + parrot[6:])
+
+# Using a step in a slice
+print(parrot[0:6:2]) #Nre
+print(parrot[0:6:3]) # Nw
